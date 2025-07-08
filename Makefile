fac4932bb6a5b5cee30986769faba2b2932bc390
NatuAls <nahuelalesso19@gmail.com>
2025-07-08 23:12:56 +0200

    modifique el Makefile para poder compilar con parsing.c y tokenizer.c y agrege testeos en main

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME = minishell
 
-SRC = main.c
+SRC = main.c tokenizer.c parsing.c
 HDR = includes/minishell.h
 
 CC = cc
--- main.c
+++ main.c
@@ -6,29 +6,46 @@ void ft_error_handler(char *s, int errorcode)
 	ft_printf("%s",s);
 	exit(errorcode);
 }
-//
 
 void ft_getinput(t_mini_sh sh)
 {
-	char *input;
+	char	*input;
 
 	(void)sh;	
 	while (1)
 	{
-				input = readline(GRN "Minishell$>" NRM);
-				if (!input)
-						ft_error_handler("input error",1);
-				if (input[0] == ' ' || input[0] =='\0')
-				{
-						free(input);
-						ft_printf("");
-				}
-				else 
-				{
-					add_history(input);
+		input = readline(GRN "Minishell$> " NRM);
+		if (!input)
+			ft_error_handler("input error",1);
+		if (input[0] == ' ' || input[0] =='\0')
+		{
+			free(input);
+			ft_printf("");
+		}
+		else 
+		{
+			add_history(input);
+			ft_printf("Has escrito " BLU "%s\n" NRM,input);
 
-					ft_printf("Has escrito " BLU "%s\n" NRM,input);
-				}
+			Token *tokens = tokenizer(input);
+			Token *tokcpy = tokens;
+			while (tokcpy)
+			{
+				printf("TOKEN %d: %s\n", tokcpy->type, tokcpy->value);
+				tokcpy = tokcpy->next;
+			}
+
+			ASTNode *node = parse_command(&tokens);
+			printf("ASTNode %d: ", node->type);
+			char **args = node->args;
+			printf("{ ");
+			while(args && *args)
+			{
+				printf("'%s' ", *args);
+				args++;
+			}
+			printf("}\n");
+		}
 	}
 
 
